778ea0f85ddeb2fdc616850bf5b32b82ddc3b0e0
karandeep <karandeep@gmail.com>
2021-09-22 15:18:56 +0530

    angular app with server

--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/tree'));
+app.use(express.static('./dist'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/tree'}),
+    res.sendFile('index.html', {root: 'dist/'}),
 );
 
 // Start the app by listening on the default Heroku port
